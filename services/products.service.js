a4aa8ad13a31a3b19d7ac85c4153086a9585a45c
sao <wllmgr@hotmail.com>
2026-04-08 17:40:36 -0500

    ** Modified file: models: customer.model, index, routes: categories, schemas: customer.schema, product.schemas services: customers.service, products.service *** new file: migrations: -product.js, -order,model: category.model, product.model, order.model, routers: orders.router, schemas: category.schema, order.schema, service: category.service, order.service

--- services/products.service.js
+++ services/products.service.js
@@ -3,7 +3,7 @@ const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
 
-const sequelize = require('../libs/sequelize');
+const { models } = require('../libs/sequelize');
 
 
 class ProductsService {
@@ -28,23 +28,19 @@ class ProductsService {
   }
 
   async create(data) {
-    const newProduct = {
-      id: faker.string.uuid(),
-      ...data
-    }
-    this.products.push(newProduct);
+    const newProduct = await models.Product.create(data);
     return newProduct;
   }
 
-
   async find() {
-    const query = 'SELECT * FROM tasks';
     try {
-      const [data] = await sequelize.query(query);
-      return data;
+      const products = await models.Product.findAll({
+        include: ['category']
+      });
+      return products;
     } catch (error) {
-      console.error('Error in query:', error);
-      throw boom.internal('Database query failed');
+      console.error('Error in products query:', error);
+      throw boom.internal('Database error');
     }
   }
 
